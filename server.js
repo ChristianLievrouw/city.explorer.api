928168eefd9fcec78a82f997f494ca6620642ded
ChristianLievrouw <celievrouw1@gmail.com>
2020-07-28 09:31:13 -0500

    cleaned up server.js

--- server.js
+++ server.js
@@ -9,34 +9,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Route Definitions
-app.use(express.static('./public'));
-app.get('/greet', greetHandler);
-app.get('/data', dataHandler);
-app.get('/error', (request, response) => {
-  throw new Error('Try again later');
-});
+app.get('/', rootHandler);
 app.use('*', notFoundHandler);
 app.use(errorHandler);
 
 // Route Handlers
-function greetHandler(request, response) {
-  response.status(200).send('Hello!');
+function rootHandler(request, response){
+  response.status(200).send('City Explorer back-end')
 }
 
-function dataHandler(request, response) {
-  const airplanes = [
-    {
-      departure: new Date(2020, 7, 24),
-      canFly: true,
-      pilot: 'Well Trained'
-    },
-    {
-      departure: new Date(2020, 7, 25),
-      canFly: false,
-      pilot: 'NA'
-    }
-  ];
-  response.status(200).json(airplanes);
+function greetHandler(request, response) {
+  response.status(200).send('Hello!');
 }
 
 function notFoundHandler(request, response) {
